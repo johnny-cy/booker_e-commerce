8d83813369adea64f22acfb179342d93cfb0c090
unknown <user@SFA021910001.sfcl.com.tw>
2021-02-19 23:41:14 +0800

    debug myadmin

--- myobject/myadmin/views/goods.py
+++ myobject/myadmin/views/goods.py
@@ -122,7 +122,7 @@ def insert(request):
         if not myfile:
             return HttpResponse("尚未上传雜誌封面圖！")
         # 以时间戳命名一个新图片名称
-        filename= str(time.time())+".jpg"
+        filename= str(time.time())+"_.jpg"
         print(filename)
         destination = open(os.path.join("./static/goods/",filename),'wb+')
         for chunk in myfile.chunks():      # 分块写入文件  
