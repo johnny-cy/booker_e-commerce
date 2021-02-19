5594e24e6bf9206b2979a556bbbf44a1ed3d21f3
unknown <user@SFA021910001.sfcl.com.tw>
2021-02-19 23:44:36 +0800

    debug myadmin

--- myobject/myadmin/views/goods.py
+++ myobject/myadmin/views/goods.py
@@ -123,6 +123,7 @@ def insert(request):
             return HttpResponse("尚未上传雜誌封面圖！")
         # 以时间戳命名一个新图片名称
         filename= str(time.time())+"_.jpg"
+        print("time.time:", time.time())
         print("222")
         print(filename)
         destination = open(os.path.join("./static/goods/",filename),'wb+')
