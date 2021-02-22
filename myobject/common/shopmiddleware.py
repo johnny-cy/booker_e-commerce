fffdb2cd335daaf59c4f60837f7c408246059aa3
unknown <user@SFA021910001.sfcl.com.tw>
2021-02-22 11:28:31 +0800

    debug

--- myobject/common/shopmiddleware.py
+++ myobject/common/shopmiddleware.py
@@ -11,11 +11,11 @@ class MyadminPagesMiddleware(object):
 	def __call__(self,request):
 		
 		# 定义网站后台不用登录也可访问的路由url
-		urllist = ['/myadmin/login','/myadmin/dologin','/myadmin/logout','/myadmin/verify']
+		# urllist = ['/myadmin/login','/myadmin/dologin','/myadmin/logout','/myadmin/verify']
 		# 获取当前请求路径
 		path = request.path
 		# 判断当前请求是否是访问网站后台,并且path不在urllist中
-		if re.match("^/myadmin/",path) and path not in urllist:
+		if re.match("^/booker/myadmin/",path) and path not in urllist:
 			# 判断当前用户是否没有登录
 			if 'adminuser' not in request.session:
 				return redirect(reverse('myadmin_login'))
