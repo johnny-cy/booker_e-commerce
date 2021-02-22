c0cccf676c0f7deb230074a96e75477379928cc7
unknown <user@SFA021910001.sfcl.com.tw>
2021-02-22 12:20:28 +0800

    debug

--- myobject/common/shopmiddleware.py
+++ myobject/common/shopmiddleware.py
@@ -46,6 +46,7 @@ class VIPPagesMiddleware(object):
 				return redirect(reverse('login')) # 順便回傳當前的request.path，登入之後方便直接回來
 		elif re.match('^/booker/myadmin/', path):
 			if re.match('^/booker/myadmin/varify$', path):
+				print("it is varify, do not redirect.")
 				pass
 			elif 'adminuser' not in request.session:
 				return render(request,'myadmin/login.html')
