bcd602e3ac0c16e8413387daf692c9c0af7682cd
unknown <user@SFA021910001.sfcl.com.tw>
2021-02-22 11:24:07 +0800

    debug

--- myobject/common/shopmiddleware.py
+++ myobject/common/shopmiddleware.py
@@ -15,7 +15,7 @@ class ShopMiddleware(object):
 		# 获取当前请求路径
 		path = request.path
 		# 判断当前请求是否是访问网站后台,并且path不在urllist中
-		if re.match("/myadmin",path) and path not in urllist:
+		if re.match("/myadmin/",path) and path not in urllist:
 			# 判断当前用户是否没有登录
 			if 'adminuser' not in request.session:
 				return redirect(reverse('myadmin_login'))
@@ -38,11 +38,15 @@ class FeedbackMiddleware(object):
 		# 获取当前请求路径
 		path = request.path
 		# 判断当前path是否在urllist中
-		if path in urllist:
-			# 判断当前用户是否有登录，若沒則跳轉登入頁面 
+		if re.match('^/vip/', path):
 			if 'vipuser' not in request.session:
 				print("vipuser not in session..redirect to login")
 				return redirect(reverse('login')) # 順便回傳當前的request.path，登入之後方便直接回來
+		# if path in urllist:
+		# 	# 判断当前用户是否有登录，若沒則跳轉登入頁面 
+		# 	if 'vipuser' not in request.session:
+		# 		print("vipuser not in session..redirect to login")
+		# 		return redirect(reverse('login')) # 順便回傳當前的request.path，登入之後方便直接回來
 		
 		response = self.get_response(request)
 		
